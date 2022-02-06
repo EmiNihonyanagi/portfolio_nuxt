221d5c764f0dc74352945e2e3b7e55a5e1e195cd
EmiNihonyanagi <emipuu33@gmail.com>
2022-02-06 14:09:58 +0900

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  div
+    <h1>title</h1>
+    section.home__profile(id="home__profile") a
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  color: $colorCorporateMain;
+}
+</style>
